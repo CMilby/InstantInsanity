0cabf6e848d7e115760c3632ad51e4fcc7e85ca5
CMilby <cmilby99@gmail.com>
2016-01-07 17:04:45 -0500

    Initial Commit

---
+++ InstantInsanity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstantInsanity
+//
+//  Created by Craig Milby on 1/7/16.
+//  Copyright © 2016 Craig Milby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstantInsanity/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  InstantInsanity
+//
+//  Created by Craig Milby on 1/7/16.
+//  Copyright © 2016 Craig Milby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
